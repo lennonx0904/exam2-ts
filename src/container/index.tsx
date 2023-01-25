582023b111ae5c2417c0142c10fbaf19aabac196
Andi <andi.lin.hy@gmail.com>
2023-01-25 13:50:11 +0900

    feat: store card component

---
+++ src/container/index.tsx
@@ -0,0 +1,13 @@
+import { StoreCard } from "components";
+
+import "./style.scss";
+
+const MainPage = () => {
+  return (
+    <div>
+      <StoreCard />
+    </div>
+  );
+};
+
+export default MainPage;
